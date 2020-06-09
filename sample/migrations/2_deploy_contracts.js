c331e2178ef6d1f0c53d436ff2d7642988a7925c
Daniel Briskin <daniel.briskin@gmail.com>
2020-06-09 14:42:04 +0900

    Initial commit

---
+++ sample/migrations/2_deploy_contracts.js
@@ -0,0 +1,16 @@
+const ConvertLib = artifacts.require("ConvertLib");
+const MetaCoin = artifacts.require("MetaCoin");
+
+const { Deployer } = require("truffle-multibaas-plugin");
+
+module.exports = async function (_deployer, network) {
+  const deployer = new Deployer(_deployer, network);
+  await deployer.setup();
+
+  await deployer.deploy(ConvertLib, { overwrite: false });
+  await deployer.link(ConvertLib, MetaCoin);
+  await deployer.deployWithOptions({
+    contractVersion: "2.0",
+    addressLabel: "metacoin",
+  }, MetaCoin);
+};
